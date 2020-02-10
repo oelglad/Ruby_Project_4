fcc6822bc40f6d806394b95fea00f8fd293388e4
oelglad <omnia_elglad@aol.com>
2020-02-10 11:18:17 -0500

    Create User table[user.rb]

---
+++ reservation_rest/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+     # encrypt password
+    has_secure_password
+    has_many :reservation, foreign_key: :created_by
+    # Validations
+    validates :name, presence: true
+    validates :email, uniqueness: true, presence: true
+    validates :password_digest, presence: true
+end
