5cd4352534dd220aa136476d770eb0ab1a836761
oelglad <omnia_elglad@aol.com>
2020-02-11 14:46:01 -0500

    [Header.js]

--- reservation_rest/client/src/components/Header.js
+++ reservation_rest/client/src/components/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
 
 
 // import CreateJob from "./createJob";
@@ -37,9 +39,8 @@ class Header extends Component {
           <ul>
           <li className="left"><Link to="/home">Home</Link></li>
           <li className="left"><Link to="/reservation">Reservation</Link></li>
-          <li className="left"><Link to="/about">About</Link></li>
-          {/* <Link to="/login">Login / Register</Link> */}
-
+          <li className="left"><Link to="/overview">Overview</Link></li>
+          
           {this.props.currentUser ? (
             <div>
               <Link to="/reservation">Reservation</Link>
@@ -47,7 +48,30 @@ class Header extends Component {
             </div>
           ) : (
             <div></div>
-           
+            )}
+            {!this.state.register ?  (
+              <li className="right"><LoginForm
+              handleRegister={this.handleRegister}
+              handleLogin={this.props.handleLogin}
+              />
+              </li>
+            ) : (
+              <li className="right"><SignUpForm
+              handleBack={this.handleBack}
+              />
+              </li>
+            )}
+            {this.props.currentUser ? (
+            <div>
+             <p className="welcome">Hello .     
+                {this.props.currentUser.username}</p>
+                <li className="right"><Link to="/reservation">Post Jobs</Link></li>
+
+                <li className="right"><button onClick={this.props.handleLogout}>Logout</button></li>
+                </div>
+          ) : (
+            <div></div>
+            
             )}
          </ul>
         </div>
