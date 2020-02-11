6e5a8e9f84e10fcb4267c096477847fc622de149
oelglad <omnia_elglad@aol.com>
2020-02-11 14:48:36 -0500

    authentication_controller.rb

--- reservation_rest/app/controllers/authentication_controller.rb
+++ reservation_rest/app/controllers/authentication_controller.rb
@@ -1,13 +1,13 @@
 class AuthenticationController < ApplicationController
     skip_before_action :authorize_request, only: :authenticate
+  
     # return auth token once user is authenticated
     def authenticate
       auth_token =
           AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
-         response= json_response(auth_token: auth_token), email:auth_params[:email])
-          # json_response(response, :created)
+      user = User.find_by(email: auth_params[:email])
+      json_response(user: user, auth_token: auth_token)
     end
-  
   
     private
   
