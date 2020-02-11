c95871656438e8d070034ba7f8ae931e01f541d2
oelglad <omnia_elglad@aol.com>
2020-02-11 14:50:10 -0500

    [schema.rb]

--- reservation_rest/db/schema.rb
+++ reservation_rest/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_152221) do
+ActiveRecord::Schema.define(version: 2020_02_11_143052) do
+
+  create_table "reservations", force: :cascade do |t|
+    t.string "reservation_desc"
+    t.string "room_type"
+    t.integer "guest_no"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
