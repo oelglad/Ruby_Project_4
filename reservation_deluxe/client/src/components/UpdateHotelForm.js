670f0a881f686f98344e6895ffd09e9cd5075100
oelglad <omnia_elglad@aol.com>
2020-02-14 11:13:51 -0500

    [UpdatedHotelForm.js]

---
+++ reservation_deluxe/client/src/components/UpdateHotelForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+export default class UpdateHotelForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hotel_name: ""
+    }
+  }
+
+  setFormData = () => {
+    if (this.props.hotels.length) {
+      console.log(this.props.hotels);
+      const { hotel_name } = this.props.hotels.find(hotel => {
+        return parseInt(hotel.id) === parseInt(this.props.hotelId)
+      })
+      this.setState({hotel_name})
+    }
+  }
+
+  componentDidMount() {
+    this.setFormData();
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.hotels !== this.props.hotels) {
+      this.setFormData();
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        this.props.updateHotel(this.props.hotelId, this.state)
+      }}>
+        <label htmlFor="hotel_name">Hotel</label>
+        <input
+          type="text"
+          name="hotel_name"
+          value={this.state.hotel_name}
+          onChange={this.handleChange}
+        />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
