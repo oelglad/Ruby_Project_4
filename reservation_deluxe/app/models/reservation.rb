ef4d854c4b2a652c4c3c6980fe63b5652b5a94ee
oelglad <omnia_elglad@aol.com>
2020-02-18 15:45:43 -0500

    update [reservation.rb]

--- reservation_deluxe/app/models/reservation.rb
+++ reservation_deluxe/app/models/reservation.rb
@@ -1,5 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :hotel
 
-  validates_presence_of :reservation_desc
+  validates_presence_of :reservation_desc, :room_type, :guest_no, :start_date
+
 end
