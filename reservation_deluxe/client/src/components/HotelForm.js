75496619aa4c2bef8cda8fdcfa3d671bed255d53
oelglad <omnia_elglad@aol.com>
2020-02-18 15:50:19 -0500

    update [HotelForm.js]

--- reservation_deluxe/client/src/components/HotelForm.js
+++ reservation_deluxe/client/src/components/HotelForm.js
@@ -6,8 +6,15 @@ export default class HotelForm extends Component {
     super(props);
 
     this.state = {
-      hotel_name: ""
-    }
+      hotels :[{
+      hotel_name: "",
+      city: "",
+      state: "",
+      zip_code: "",
+      biography: "",
+      img_url: ""
+    }]
+  }
   }
 
   handleChange = (e) => {
@@ -17,19 +24,54 @@ export default class HotelForm extends Component {
 
   render() {
     return (
-      <div>
-      <form onSubmit={(e) => {
+      <div >
+      <form className="hotelForm"  onSubmit={(e) => {
         e.preventDefault();
         this.props.createHotel(this.state)
       }}>
-        <label htmlFor="hotel_name">Hotel</label>
+        <label className="addHotel" htmlFor="hotel_name">Hotel</label>
         <input
           type="text"
           name="hotel_name"
           value={this.state.hotel_name}
           onChange={this.handleChange}
         />
-        <button>Submit</button>
+        <label htmlFor="city">City</label>
+        <input
+          type="text"
+          name="city"
+          value={this.state.city}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="state">State</label>
+        <input
+          type="text"
+          name="state"
+          value={this.state.state}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="zip_code">Zip-code</label>
+        <input
+          type="text"
+          name="zip_code"
+          value={this.state.zip_code}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="biography">Biography</label>
+        <input
+          type="text"
+          name="biography"
+          value={this.state.biography}
+          onChange={this.handleChange}
+        />
+         <label htmlFor="img_url">img_url</label>
+        <input className="HotelImg" 
+          type="text"
+          name="img_url"
+          value={this.state.img_url}
+          onChange={this.handleChange}
+        />
+        <button id="submitButton">Submit</button>
       </form>
       </div>
     )
