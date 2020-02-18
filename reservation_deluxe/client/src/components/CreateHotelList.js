cf18308e581ae31088883d8a7ddf770cd531bc19
oelglad <omnia_elglad@aol.com>
2020-02-18 15:48:04 -0500

    update [CreateHotelList.js]

--- reservation_deluxe/client/src/components/CreateHotelList.js
+++ reservation_deluxe/client/src/components/CreateHotelList.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
+import ReservationContainer from './ReservationContainer';
 
 
-export default class CreateHotelList extends Component { 
-  constructor(props){
+export default class CreateHotelList extends Component {
+  constructor(props) {
     super(props);
   }
 
-  render(){
+  render() {
     console.log(this.props.hotels)
-  return (
-    <div>
-      {this.props.hotels && this.props.hotels.map(hotel => (
-        <div key={hotel.id}>
-          <Link to={`/hotels/${hotel.id}`}>{hotel.hotel_name}</Link>
+    return (
+      <div>
+        <Link className="addHotel" to="/hotels/new"><button>Add a Hotel To Your List</button></Link>
+        <div id="hotel-display">
+
+          {this.props.hotels && this.props.hotels.map(hotel => (
+            <div key={hotel.id} className="hotelContainer">
+              <Link to={`/hotels/${hotel.id}`}>
+                <img className="HotelImg" src={hotel.img_url}></img>
+
+                {hotel.hotel_name}
+                <Link to={`/hotels/${hotel.id}/reservations`}></Link>
+              </Link>
+              <p>{hotel.biography}</p>
+              <p>{hotel.city}, {hotel.state}, {hotel.zip_code}</p>
+
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  )
-}
+      </div>
+    )
   }
+}
