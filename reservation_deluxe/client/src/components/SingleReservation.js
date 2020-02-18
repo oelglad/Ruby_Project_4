9ff284cae5cfb880356a5ac5cd9d0b177a81c3ae
oelglad <omnia_elglad@aol.com>
2020-02-18 16:15:07 -0500

    Update [SingleReservation.js]

--- reservation_deluxe/client/src/components/SingleReservation.js
+++ reservation_deluxe/client/src/components/SingleReservation.js
@@ -1,40 +1,56 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import React, { Component } from 'react';
+// import { Link, Route } from 'react-router-dom';
+// import { indexReservations } from '../services/api_helper';
 
-export default class SingleReservation extends Component {
-  constructor(props) {
-    super(props);
+// export default class SingleReservation extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      currentReservation: null
-    }
-  }
+//     this.state = {
+//       currentReservation: null,
+    
 
-  setCurrenteservation = () => {
-    const currentReservation = this.props.Reservations.find(reservation => reservation.id === parseInt(this.props.reservationId));
-    this.setState({ currentReservation })
-  }
+//     }
+//   }
 
-  componentDidMount() {
-    this.setCurrentReservation();
-  }
+//   // setCurrenteservation = async () => {
+//   //   const currentReservation = this.props.Reservations.find(reservation => reservation.id === parseInt(this.props.reservationId));
+//   //   this.setState({ currentReservation })
+//   // }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.reservationId !== this.props.reservationId) {
-      this.setCurrentReservation();
-    }
-  }
+//   setCurrenteservation  = async () => {
+//   }
+//   // componentDidMount = async () => {
+//   //   this.setCurrentReservation();
+//   // }
+//   componentDidMount = async () => {
+//     if (this.props.reservations.length > 0) {
+//       const currentReservation = this.props.reservations.find(reservation => parseInt(reservation.id) === parseInt(this.props.reservationId));
+//       const updateReservations = await putReservation(currentReservation.id);
+//       console.log(updateReservation)
+//       this.setState({ 
+       
+//         currentReservation: updateReservations
+//       });
+//     }
+//   }
+//   componentDidUpdate(prevProps) {
+//     if (prevProps.reservationId !== this.props.reservationId) {
+//       this.setCurrentReservation();
+//     }
+//   }
 
-  render() {
-    return (
-      <div>
-        {this.state.currentReservation && (
-          <>
-            <h1>{this.state.currentReservation.reservation_desc}</h1>
-            <Link to={`/reservations/${this.state.currentReservation.id}/edit`}><button>Edit</button></Link>
-          </>
-        )}
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         {this.state.currentReservation && (
+//           <>
+//             <h1>{this.state.currentReservation.reservation_desc}</h1>
+//             <Link to={`/hotels/${this.props.hotelId}/reservations/${this.state.currentReservation.id}/edit`}><button>Edit</button></Link>
+            
+//           </>
+//         )}
+//       </div>
+//     )
+//   }
+// }
