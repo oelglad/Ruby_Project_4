536532d96a65a87e5716a04896f56bf4bdfe14f9
oelglad <omnia_elglad@aol.com>
2020-02-18 16:10:40 -0500

    update [HotelsContainer.js]

--- reservation_deluxe/client/src/components/HotelsContainer.js
+++ reservation_deluxe/client/src/components/HotelsContainer.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import { Route, withRouter, Link } from 'react-router-dom';
+import { Route, withRouter, Switch } from 'react-router-dom';
 import { indexHotels, postHotel , putHotel, verifyUser } from '../services/api_helper';
 import CreateHotelList from './CreateHotelList';
 import SingleHotel from './SingleHotel';
 import HotelForm from './HotelForm';
-import UpdateHotelForm from './UpdateHotelForm';
+
 
 class HotelContainer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      hotels: []
+      hotels: [{
+      hotel_name: "",
+      city: "",
+      state: "",
+      zip_code: "",
+      biography: "",
+      img_url: ""
+      }]
     }
   }
 
@@ -20,13 +27,11 @@ class HotelContainer extends Component {
     this.getAllHotels();
   }
 
-  // READ ALL THE TODOS
   getAllHotels = async () => {
     const hotels = await indexHotels();
     this.setState({ hotels});
   }
 
-  //CREATE THE TODO
   createHotel = async (hotelData) => {
     console.log(hotelData);
     const newHotel = await postHotel(hotelData);
@@ -36,42 +41,36 @@ class HotelContainer extends Component {
     this.props.history.push("/hotels");
   }
 
-  // UPDATE THE TODO
-  updateHotel = async (id, hotelData) => {
-    const updatedHotel = await putHotel(id, hotelData);
-    const changedHotels = this.state.hotels.map(hotel => parseInt(hotel.id) === parseInt(id) ? updatedHotel : hotel);
-    console.log(changedHotels);
-    this.setState({ hotels: changedHotels });
-    this.props.history.push("/hotels");
-  }
+  // updateHotel = async (id, hotelData) => {
+  //   const updatedHotel = await putHotel(id, hotelData);
+  //   const changedHotels = this.state.hotels.map(hotel => parseInt(hotel.id) === parseInt(id) ? updatedHotel : hotel);
+  //   console.log(changedHotels);
+  //   this.setState({ hotels: changedHotels });
+  //   this.props.history.push("/hotels");
+  // }
 
   render() {
     return (
       <div>
-        <Route exact path="/hotels" render={() => (
-          <CreateHotelList
-            hotels={this.state.hotels}
-          />
-        )} />
-        <Route exact path="/hotels/:id" render={(props) => (
-          <SingleHotel
-            hotelId={props.match.params.id}
-            hotels={this.state.hotels}
-          />
-        )} />
-        <Link to="/hotels/new">Add a Hotel</Link>
-        <Route path="/hotels/new" render={() => (
-          <HotelForm
-            createHotel={this.createHotel}
-          />
-        )} />
-        <Route path="/hotels/:id/edit" render={(props) => (
-          <UpdateHotelForm
-          hotels={this.state.hotels}
-            updatehotel={this.updateHotel}
-            hotelId={props.match.params.id}
-          />
-        )} />
+        <Switch>
+          <Route exact path="/hotels" render={() => (
+            <CreateHotelList
+              hotels={this.state.hotels}
+            />
+          )} />
+          <Route exact path="/hotels/new" render={() => (
+            <HotelForm
+              createHotel={this.createHotel}
+            />
+          )} />
+          <Route exact path="/hotels/:id" render={(props) => (
+            <SingleHotel
+              hotelId={props.match.params.id}
+              hotels={this.state.hotels}
+            />
+            
+          )} />
+        </Switch>
       </div>
     )
   }
