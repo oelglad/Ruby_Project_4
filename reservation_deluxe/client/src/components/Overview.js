cc889d777fd35132bfe66c8de22d8c9a366ea996
oelglad <omnia_elglad@aol.com>
2020-02-18 16:12:05 -0500

    update [Overview.js]

--- reservation_deluxe/client/src/components/Overview.js
+++ reservation_deluxe/client/src/components/Overview.js
@@ -3,19 +3,16 @@ const Overview = () => {
 
   return (
     <div className="border">
-      <div id="about"></div>
-      <div id="about-mobile"></div>
       <h1 className="hours">Overview</h1>
-      <div className="AboutContainer">
+      <div className="overviewContainer">
         <div className="leftSide" >
+          <img className="imgClass" src="https://i.imgur.com/3nuNGMZ.jpg"></img>
         </div>
         <div className="rightSide">
           <p className="p3">
             Our Company has been Established in 1990, build in three locations namely Cairo in Egypt, Rome in Italy
             and NYC in USA, helping our customers to get the best hotel reservation by focusing on
             quality, luxurious yet affordable.
-
-            
           </p>
         </div>
       </div>
